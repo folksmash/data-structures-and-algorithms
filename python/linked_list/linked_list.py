1a1131d7fede812c1f89e063163eef33d68113bd
Jordan Folk <jordan.folk7@gmail.com>
2021-12-18 13:01:59 -0800

    added code for the challenge and updated tests to meet requirements

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -10,3 +10,88 @@ class LinkedList:
     """
     def __init__(self):
         self.head = None
+
+
+
+    def insert(self, value):
+        self.head = Node(value, self.head)
+
+
+    def append(self, value):
+        current = self.head
+        if current:
+            while current.next != None:
+                current = current.next
+            current.next = Node(value)
+        else:
+            self.head = Node(value, self.head)
+
+
+    def insert_before(self, value, position):
+        new = Node(value)
+        pointer = self.head
+        counter = 1
+        if position == 0:
+            new.next = self.head
+            self.head = new
+            return self.head
+        while pointer.next is not None:
+            if counter == position:
+                new.next = pointer.next
+                pointer.next = new
+                break
+            counter += 1
+            pointer = pointer.next
+        return self.head
+
+
+    def insert_after(self, value, position):
+        new = Node()
+        pointer = self.head
+        counter = 1
+        if position == 0:
+            new.next = self.head
+            self.head = new
+            return self.head
+        while pointer.next is not None:
+            if counter == position:
+                new.next = pointer.next
+                pointer.next = new
+                break
+            counter += 1
+            pointer = pointer.next
+        return self.head
+
+    def includes(self, value):
+        current = self.head
+
+        while current:
+            if current.values == value:
+                return True
+            current = current.next
+        return False
+
+    def __str__(self):
+        current = self.head
+        output = ""
+        while current:
+            output += "{ " + current.values + " } -> "
+            current = current.next
+        output += "NULL"
+        return output
+
+    def kth_value(self, k):
+        current_node = self.head
+        found_node = self.head
+        dist = 0
+
+        while current_node:
+            if dist > k:
+                found_node = found_node.next
+            current_node = current_node.next
+            dist += 1
+
+        if dist > k:
+            return found_node.values
+        else:
+            raise ValueError("the list is shorter than the given value")
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,108 @@
-from linked_list.linked_list import LinkedList
+from linked_list.linked_list import LinkedList, Node
+import pytest
 
 def test_import():
     assert LinkedList
 
+def test_empty_list():
+    empty_list = LinkedList()
+    assert empty_list.head == None
+
+def test_node_exists():
+    assert Node
+
+def test_create_node():
+    new_node = Node('stuff')
+    assert new_node.values == 'stuff'
+    assert new_node.next == None
+
+def test_insert_node():
+    new_head = Node('stuff')
+    assert new_head.values == 'stuff'
+    assert new_head.next == None
+
+
+def test_includes_true():
+    lst = LinkedList()
+    lst.insert("dog")
+    lst.insert("cat")
+    actual = lst.includes("dog")
+    expected = True
+    assert actual == expected
+
+
+def test_includes_false():
+    lst = LinkedList()
+    lst.insert("dog")
+    lst.insert("cat")
+    actual = lst.includes("ball")
+    expected = False
+    assert actual == expected
+
+def test_to_string():
+    lst = LinkedList()
+    lst.insert("dog")
+    lst.insert("cat")
+    assert str(lst) == "{ cat } -> { dog } -> NULL"
+
+
+def test_append_end():
+    list = LinkedList()
+    list.insert("1")
+    list.append("2")
+    assert str(list) == "{ 1 } -> { 2 } -> NULL"
+
+def test_insert_before():
+    list = LinkedList()
+    list.insert("1")
+    list.insert("2")
+    list.insert("4")
+    list.insert("5")
+    list.insert_before("3", 2)
+    assert str(list) == "{ 5 } -> { 4 } -> { 3 } -> { 2 } -> { 1 } -> NULL"
+
+def test_insert_after():
+    list = LinkedList()
+    list.insert("1")
+    list.insert("2")
+    list.insert("4")
+    list.insert("5")
+    list.insert_before("3",2)
+    assert str(list) == "{ 5 } -> { 4 } -> { 3 } -> { 2 } -> { 1 } -> NULL"
+
+def test_append_multi_end():
+    list = LinkedList()
+    list.append("1")
+    list.append("2")
+    list.append("3")
+    assert str(list) == "{ 1 } -> { 2 } -> { 3 } -> NULL"
+
+def test_kth_greater_than():
+    list = LinkedList()
+    list.insert(1)
+    list.insert(2)
+    list.insert(3)
+    with pytest.raises(ValueError):
+        list.kth_value(4)
+
+def test_kth_same_length():
+    list = LinkedList()
+    list.insert(1)
+    list.insert(2)
+    list.insert(3)
+    with pytest.raises(ValueError):
+        list.kth_value(3)
+
+
+def test_list_size():
+    list = LinkedList()
+    list.insert(1)
+    assert list.kth_value(0) == 1
+
+def test_kth_not_positive():
+    list = LinkedList()
+    list.insert(1)
+    list.insert(2)
+    list.insert(3)
+    with pytest.raises(ValueError):
+        list.kth_value(-1)
